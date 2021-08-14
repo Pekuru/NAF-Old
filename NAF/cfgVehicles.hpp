ada38cede0ff36aa54e79154a4e942638e6ab870
Pek <9923839+Pekuru@users.noreply.github.com>
2021-08-14 10:19:41 +0200

    Wrote config

--- NAF/cfgVehicles.hpp
+++ NAF/cfgVehicles.hpp
@@ -3,10 +3,10 @@ class CfgVehicles
 	class B_Soldier_F;
 	class B_Soldier_AR_F;
 	class B_Soldier_SL_F;
-	class B_NL_Rifleman_Green: B_Soldier_F
+	class B_NAF_Soldier_NFP_Green: B_Soldier_F
 	{
 		author="Pek";
-		displayName="Rifleman (NFP G)";
+		displayName="Rifleman (NFP Green)";
 		scope=1;
 		hiddenSelections[]=
 		{
@@ -15,15 +15,15 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_uni.paa",
-			"\NLD2035\data\shirt_grey.paa"
+			"\NAF\data\uniforms\tex_uni_green.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
 		};
-		uniformClass="NLD_NFP_Green_Uni";
+		uniformClass="NAF_Uni_NFP_Green";
 	};
-	class B_NL_Rifleman_Green_Rolled: B_Soldier_SL_F
+	class B_NAF_Soldier_NFP_Green_Rolled: B_Soldier_SL_F
 	{
 		author="Pek";
-		displayName="Rifleman Rolled Sleeves (NFP G)";
+		displayName="Rifleman Rolled Sleeve (NFP Green)";
 		scope=1;
 		hiddenSelections[]=
 		{
@@ -32,15 +32,15 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_uni.paa",
-			"\NLD2035\data\shirt_grey.paa"
+			"\NAF\data\uniforms\tex_uni_green.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
 		};
-		uniformClass="NLD_NFP_Green_Rolled_Uni";
+		uniformClass="NAF_Uni_NFP_Green_Rolled";
 	};
-	class B_NL_Rifleman_Green_Shirt: B_Soldier_AR_F
+	class B_NAF_Soldier_NFP_Green_Shirt: B_Soldier_AR_F
 	{
 		author="Pek";
-		displayName="Rifleman Shirt (NFP G)";
+		displayName="Rifleman Shirt (NFP Green)";
 		scope=1;
 		hiddenSelections[]=
 		{
@@ -49,51 +49,385 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_uni.paa",
-			"\NLD2035\data\shirt_grey.paa"
+			"\NAF\data\uniforms\tex_uni_green.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
 		};
-		uniformClass="NLD_NFP_Green_Shirt_Uni";
+		uniformClass="NAF_Uni_NFP_Green_Shirt";
+	};
+	class B_NAF_Soldier_NFP_Tan: B_Soldier_F
+	{
+		author="Pek";
+		displayName="Rifleman (NFP Tan)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_tan.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_NFP_Tan";
+	};
+	class B_NAF_Soldier_NFP_Tan_Rolled: B_Soldier_SL_F
+	{
+		author="Pek";
+		displayName="Rifleman Rolled Sleeve (NFP Tan)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_tan.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_NFP_Tan_Rolled";
+	};
+	class B_NAF_Soldier_NFP_Tan_Shirt: B_Soldier_AR_F
+	{
+		author="Pek";
+		displayName="Rifleman Shirt (NFP Tan)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_tan.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_NFP_Tan_Shirt";
+	};
+	class B_NAF_Soldier_Woodland: B_Soldier_F
+	{
+		author="Pek";
+		displayName="Rifleman (Woodland)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_woodland.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
+		};
+		uniformClass="NAF_Uni_Woodland";
+	};
+	class B_NAF_Soldier_Woodland_Rolled: B_Soldier_SL_F
+	{
+		author="Pek";
+		displayName="Rifleman Rolled Sleeve (Woodland)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_woodland.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
+		};
+		uniformClass="NAF_Uni_Woodland_Rolled";
+	};
+	class B_NAF_Soldier_Woodland_Shirt: B_Soldier_AR_F
+	{
+		author="Pek";
+		displayName="Rifleman Shirt (Woodland)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_woodland.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
+		};
+		uniformClass="NAF_Uni_Woodland_Shirt";
+	};
+	class B_NAF_Soldier_Jungle: B_Soldier_F
+	{
+		author="Pek";
+		displayName="Rifleman (Jungle)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_jungle.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_Jungle";
+	};
+	class B_NAF_Soldier_Jungle_Rolled: B_Soldier_SL_F
+	{
+		author="Pek";
+		displayName="Rifleman Rolled Sleeve (Jungle)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_jungle.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_Jungle_Rolled";
+	};
+	class B_NAF_Soldier_Jungle_Shirt: B_Soldier_AR_F
+	{
+		author="Pek";
+		displayName="Rifleman Shirt (Jungle)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_jungle.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_Jungle_Shirt";
+	};
+	class B_NAF_Soldier_Desert: B_Soldier_F
+	{
+		author="Pek";
+		displayName="Rifleman (Desert)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_desert.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_Desert";
+	};
+	class B_NAF_Soldier_Desert_Rolled: B_Soldier_SL_F
+	{
+		author="Pek";
+		displayName="Rifleman Rolled Sleeve (Desert)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_desert.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_Desert_Rolled";
+	};
+	class B_NAF_Soldier_Desert_Shirt: B_Soldier_AR_F
+	{
+		author="Pek";
+		displayName="Rifleman Shirt (Desert)";
+		scope=1;
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_desert.paa",
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		uniformClass="NAF_Uni_Desert_Shirt";
 	};
 	class B_AssaultPack_rgr;
 	class B_Kitbag_rgr;
-	class B_CivilianBackpack_01_Sport_Red_F;
-	class B_NLD_Assaultpack_nfpg: B_AssaultPack_rgr
+	class B_Bergen_mcamo_F;
+	class B_NAF_AssaultPack_Desert: B_AssaultPack_rgr
 	{
 		author="Pek";
-		displayName="Assault Pack (NFP G)";
+		displayName="Assault Pack (Desert)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_bp3.paa"
+			"\NAF\data\uniforms\tex_bag_fast_desert.paa"
 		};
 	};
-	class B_NLD_Kitbag_nfpg: B_Kitbag_rgr
+	class B_NAF_AssaultPack_Jungle: B_AssaultPack_rgr
 	{
 		author="Pek";
-		displayName="Kitbag (NFP G)";
+		displayName="Assault Pack (Jungle)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_bp2.paa"
+			"\NAF\data\uniforms\tex_bag_fast_jungle.paa"
 		};
 	};
-	class B_NLD_civi_nfpg: B_CivilianBackpack_01_Sport_Red_F
+	class B_NAF_AssaultPack_multi: B_AssaultPack_rgr
 	{
 		author="Pek";
-		displayName="Cilivian Pack (NFP G)";
+		displayName="Assault Pack (NFP Multitone)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_bp1.paa"
+			"\NAF\data\uniforms\tex_bag_fast_multi.paa"
+		};
+	};
+	class B_NAF_AssaultPack_woodland: B_AssaultPack_rgr
+	{
+		author="Pek";
+		displayName="Assault Pack (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bag_fast_woodland.paa"
+		};
+	};
+	class B_NAF_Kitbag_Desert: B_Kitbag_rgr
+	{
+		author="Pek";
+		displayName="Kitbag (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bag_kit_desert.paa"
+		};
+	};
+	class B_NAF_Kitbag_jungle: B_Kitbag_rgr
+	{
+		author="Pek";
+		displayName="Kitbag (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bag_kit_jungle.paa"
+		};
+	};
+	class B_NAF_Kitbag_multi: B_Kitbag_rgr
+	{
+		author="Pek";
+		displayName="Kitbag (NFP Multitone)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bag_kit_multi.paa"
+		};
+	};
+	class B_NAF_Kitbag_Woodland: B_Kitbag_rgr
+	{
+		author="Pek";
+		displayName="Kitbag (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bag_kit_woodland.paa"
+		};
+	};
+	class B_NAF_Bergen_Desert: B_Bergen_mcamo_F
+	{
+		author="Pek";
+		displayName="Bergen (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bergen_desert.paa"
+		};
+	};
+	class B_NAF_Bergen_jungle: B_Bergen_mcamo_F
+	{
+		author="Pek";
+		displayName="Bergen (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bergen_jungle.paa"
+		};
+	};
+	class B_NAF_Bergen_multi: B_Bergen_mcamo_F
+	{
+		author="Pek";
+		displayName="Bergen (NFP Multitone)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bergen_multi.paa"
+		};
+	};
+	class B_NAF_Bergen_olive: B_Bergen_mcamo_F
+	{
+		author="Pek";
+		displayName="Bergen (Olive)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bergen_olive.paa"
+		};
+	};
+	class B_NAF_Bergen_tan: B_Bergen_mcamo_F
+	{
+		author="Pek";
+		displayName="Bergen (Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_bergen_tan.paa"
 		};
 	};
 };
--- NAF/cfgWeapons.hpp
+++ NAF/cfgWeapons.hpp
@@ -13,44 +13,44 @@ class CfgWeapons
 	{
 		class ItemInfo;
 	};
-	class NLD_NFP_Green_Uni: U_B_CombatUniform_mcam
+	class NAF_Uni_NFP_Green: U_B_CombatUniform_mcam
 	{
 		author="Pek";
-		displayName="Combat Uniform (NFP Green)";
+		displayName="BDU (NFP Green)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_uni.paa"
+			"\NAF\data\uniforms\tex_uni_green.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
-			uniformClass="B_NL_Rifleman_Green";
+			uniformClass="B_NAF_Soldier_NFP_Green";
 		};
 	};
-	class NLD_NFP_Green_Rolled_Uni: U_B_CombatUniform_mcam_vest
+	class NAF_Uni_NFP_Green_Rolled: U_B_CombatUniform_mcam_vest
 	{
 		author="Pek";
-		displayName="Combat Uniform Rolled (NFP Green)";
+		displayName="BDU Rolled (NFP Green)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_uni.paa"
+			"\NAF\data\uniforms\tex_uni_green.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
-			uniformClass="B_NL_Rifleman_Green_Rolled";
+			uniformClass="B_NAF_Soldier_NFP_Green_Rolled";
 		};
 	};
-	class NLD_NFP_Green_Shirt_Uni: U_B_CombatUniform_mcam_tshirt
+	class NAF_Uni_NFP_Green_Shirt: U_B_CombatUniform_mcam_tshirt
 	{
 		author="Pek";
-		displayName="Combat Uniform T-Shirt (NFP Green)";
+		displayName="BDU Shirt (NFP Green)";
 		hiddenSelections[]=
 		{
 			"camo",
@@ -58,27 +58,224 @@ class CfgWeapons
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_uni.paa",
-			"\NLD2035\data\shirt_grey.paa"
+			"\NAF\data\uniforms\tex_uni_green.paa",
+			"\NAF\data\uniforms\tex_shirt_grey.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
-			uniformClass="B_NL_Rifleman_Green_Shirt";
+			uniformClass="B_NAF_Soldier_NFP_Green_Shirt";
 		};
 	};
-	class HeadgearItem;
-	class H_HelmetHBK_headset_F;
-	class NLD_NFP_Green_Helmet: H_HelmetHBK_headset_F
+	class NAF_Uni_NFP_Tan: U_B_CombatUniform_mcam
 	{
 		author="Pek";
-		displayName="Helm (NFP G)";
+		displayName="BDU (NFP Tan)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_helm.paa"
+			"\NAF\data\uniforms\tex_uni_tan.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_NFP_Tan";
+		};
+	};
+	class NAF_Uni_NFP_Tan_Rolled: U_B_CombatUniform_mcam_vest
+	{
+		author="Pek";
+		displayName="BDU Rolled (NFP Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_tan.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_NFP_Tan_Rolled";
+		};
+	};
+	class NAF_Uni_NFP_Tan_Shirt: U_B_CombatUniform_mcam_tshirt
+	{
+		author="Pek";
+		displayName="BDU Shirt (NFP Tan)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_tan.paa"
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_NFP_Tan_Shirt";
+		};
+	};
+	class NAF_Uni_Woodland: U_B_CombatUniform_mcam
+	{
+		author="Pek";
+		displayName="BDU (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_woodland.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Woodland";
+		};
+	};
+	class NAF_Uni_Woodland_Rolled: U_B_CombatUniform_mcam_vest
+	{
+		author="Pek";
+		displayName="BDU Rolled (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_woodland.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Woodland_Rolled";
+		};
+	};
+	class NAF_Uni_Woodland_Shirt: U_B_CombatUniform_mcam_tshirt
+	{
+		author="Pek";
+		displayName="BDU Shirt (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_woodland.paa"
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Woodland_Shirt";
+		};
+	};
+	class NAF_Uni_Jungle: U_B_CombatUniform_mcam
+	{
+		author="Pek";
+		displayName="BDU (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_jungle.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Jungle";
+		};
+	};
+	class NAF_Uni_Jungle_Rolled: U_B_CombatUniform_mcam_vest
+	{
+		author="Pek";
+		displayName="BDU Rolled (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_jungle.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Jungle_Rolled";
+		};
+	};
+	class NAF_Uni_Jungle_Shirt: U_B_CombatUniform_mcam_tshirt
+	{
+		author="Pek";
+		displayName="BDU Shirt (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_jungle.paa"
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Jungle_Shirt";
+		};
+	};
+	class NAF_Uni_Desert: U_B_CombatUniform_mcam
+	{
+		author="Pek";
+		displayName="BDU (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_desert.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Desert";
+		};
+	};
+	class NAF_Uni_Desert_Rolled: U_B_CombatUniform_mcam_vest
+	{
+		author="Pek";
+		displayName="BDU Rolled (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_desert.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Desert_Rolled";
+		};
+	};
+	class NAF_Uni_Desert_Shirt: U_B_CombatUniform_mcam_tshirt
+	{
+		author="Pek";
+		displayName="BDU Shirt (Desert)";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo2"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\uniforms\tex_uni_desert.paa"
+			"\NAF\data\uniforms\tex_shirt_brown.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			uniformClass="B_NAF_Soldier_Desert_Shirt";
 		};
 	};
 	class Vest_Camo_Base;
@@ -87,17 +284,25 @@ class CfgWeapons
 	{
 		class ItemInfo;
 	};
-	class NLD_NFP_Green_Platecarrier: V_PlateCarrier1_rgr
+	class V_PlateCarrier2_rgr: Vest_NoCamo_Base
+	{
+		class ItemInfo;
+	};
+	class V_PlateCarrierSpec_rgr: Vest_NoCamo_Base
+	{
+		class ItemInfo;
+	};
+	class V_NAF_PC_Lite_Multi: V_PlateCarrier1_rgr
 	{
 		author="Pek";
-		displayName="Platecarrier (NFP G)";
+		displayName="PC Lite (NFP Multitone)";
 		hiddenSelections[]=
 		{
 			"camo"
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"\NLD2035\data\nfp_green_pc.paa"
+			"\NAF\data\gear\tex_pc_lite_multi.paa"
 		};
 		class ItemInfo: ItemInfo
 		{
@@ -105,6 +310,392 @@ class CfgWeapons
 			{
 				"camo"
 			};
+		};
+	};
+	class V_NAF_PC_Rig_Multi: V_PlateCarrier2_rgr
+	{
+		author="Pek";
+		displayName="PC Rig (NFP Multitone)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_multi.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Rig_Spec_Multi: V_PlateCarrierSpec_rgr
+	{
+		author="Pek";
+		displayName="PC Rig Special (NFP Multitone)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_multi.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Lite_Olive: V_PlateCarrier1_rgr
+	{
+		author="Pek";
+		displayName="PC Lite (Olive)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_olive.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Rig_Olive: V_PlateCarrier2_rgr
+	{
+		author="Pek";
+		displayName="PC Rig (Olive)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_olive.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Rig_Spec_Olive: V_PlateCarrierSpec_rgr
+	{
+		author="Pek";
+		displayName="PC Rig Special (Olive)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_olive.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Lite_Tan: V_PlateCarrier1_rgr
+	{
+		author="Pek";
+		displayName="PC Lite (Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_tan.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Rig_Tan: V_PlateCarrier2_rgr
+	{
+		author="Pek";
+		displayName="PC Rig (Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_tan.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class V_NAF_PC_Rig_Spec_Tan: V_PlateCarrierSpec_rgr
+	{
+		author="Pek";
+		displayName="PC Rig Special (Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_pc_hev_tan.paa"
+		};
+		class ItemInfo: ItemInfo
+		{
+			hiddenSelections[]=
+			{
+				"camo"
+			};
+		};
+	};
+	class HeadgearItem;
+	class H_HelmetHBK_headset_F;
+	class H_HelmetIA;
+	class H_MilCap_grn;
+	class H_Booniehat_khk_hs;
+	class H_NAF_Viper_Green: H_HelmetHBK_headset_F
+	{
+		author="Pek";
+		displayName="Viper Helmet (NFP Green)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_viper_green.paa"
+		};
+	};
+	class H_NAF_Viper_Tan: H_HelmetHBK_headset_F
+	{
+		author="Pek";
+		displayName="Viper Helmet (NFP Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_viper_tan.paa"
+		};
+	};
+	class H_NAF_MICH_Desert: H_HelmetIA
+	{
+		author="Pek";
+		displayName="MICH2001 (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_mich_desert.paa"
+		};
+	};
+	class H_NAF_MICH_Jungle: H_HelmetIA
+	{
+		author="Pek";
+		displayName="MICH2001 (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_mich_jungle.paa"
+		};
+	};
+	class H_NAF_MICH_Woodland: H_HelmetIA
+	{
+		author="Pek";
+		displayName="MICH2001 (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_mich_woodland.paa"
+		};
+	};
+	class H_NAF_Cap_Desert: H_MilCap_grn
+	{
+		author="Pek";
+		displayName="Cap (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_cap_desert.paa"
+		};
+	};
+	class H_NAF_Cap_Jungle: H_MilCap_grn
+	{
+		author="Pek";
+		displayName="Cap (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_cap_jungle.paa"
+		};
+	};
+	class H_NAF_Cap_Woodland: H_MilCap_grn
+	{
+		author="Pek";
+		displayName="Cap (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_cap_woodland.paa"
+		};
+	};
+	class H_NAF_Cap_Green: H_MilCap_grn
+	{
+		author="Pek";
+		displayName="Cap (NFP Green)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_cap_green.paa"
+		};
+	};
+	class H_NAF_Cap_Tan: H_MilCap_grn
+	{
+		author="Pek";
+		displayName="Cap (NFP Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_cap_tan.paa"
+		};
+	};
+	class H_NAF_Cap_Multi: H_MilCap_grn
+	{
+		author="Pek";
+		displayName="Cap (NFP Multitone)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_cap_multi.paa"
+		};
+	};
+	class H_NAF_Boonie_Desert: H_Booniehat_khk_hs
+	{
+		author="Pek";
+		displayName="Boonie (Desert)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_boonie_desert.paa"
+		};
+	};
+	class H_NAF_Boonie_Jungle: H_Booniehat_khk_hs
+	{
+		author="Pek";
+		displayName="Boonie (Jungle)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_boonie_jungle.paa"
+		};
+	};
+	class H_NAF_Boonie_Woodland: H_Booniehat_khk_hs
+	{
+		author="Pek";
+		displayName="Boonie (Woodland)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_boonie_woodland.paa"
+		};
+	};
+	class H_NAF_Boonie_Green: H_Booniehat_khk_hs
+	{
+		author="Pek";
+		displayName="Boonie (NFP Green)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_boonie_green.paa"
+		};
+	};
+	class H_NAF_Boonie_Tan: H_Booniehat_khk_hs
+	{
+		author="Pek";
+		displayName="Boonie (NFP Tan)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_boonie_tan.paa"
+		};
+	};
+	class H_NAF_Boonie_Multi: H_Booniehat_khk_hs
+	{
+		author="Pek";
+		displayName="Boonie (NFP Multitone)";
+		hiddenSelections[]=
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\NAF\data\gear\tex_boonie_multi.paa"
 		};
 	};
 };
